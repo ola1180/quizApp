3a7f0af2a7e548c3c3496ab315701b7121b020bf
Ola <aleksandra.kurdel@gmail.com>
2023-09-11 14:03:18 +0200

    code refactoring

--- app.js
+++ app.js
@@ -1,104 +1,106 @@
 let quiz = document.querySelector('.quiz-data')
 const apiUrl = 'https://opentdb.com/api.php?amount=10'
 const button = document.querySelector('.quizButton')
-let pageCounter = 0
+let pageCounter = 1
 let answerCounter = 0
 let correctIndex = 0
-
-
+let slideIndex = 0
 
 async function init(apiUrl) {
 
     fetch(apiUrl)
         .then(response => {
-
             if (response.status === 200) {
                 return response.json()
             }
             throw new Error()
         })
         .then((dataFromApi) => {
-            renderQuestions(dataFromApi.results[pageCounter])
+            renderQuestions(dataFromApi.results[slideIndex])
             nextSlide(dataFromApi)
         })
         .catch((error) => showError())
 
 }
 
-
-
 function renderQuestions({ question, correct_answer, incorrect_answers: [...allIncorrectAnswers] }) {
 
+    allAnswers = [correct_answer, ...allIncorrectAnswers]
 
-    let allQuestions = [correct_answer, ...allIncorrectAnswers]
+    allAnswers.forEach((item, index) => {
+        // (index === 0) ? { answer: item, isCorrect: true } : { answer: item, isCorrect: false }
 
-
-
-    for (i = allQuestions.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        let memory = allQuestions[i]
-        allQuestions[i] = allQuestions[j]
-        allQuestions[j] = memory
-    }
-
-
-    let correctIndex = allQuestions.indexOf(correct_answer)
-
-    let view = `<ul>  ${question}`
-
-    for (question of allQuestions) {
-        view += `<li>${question}</li>`
-    }
-
-    view += `</ul>`
-    quiz.innerHTML = view
-
-    checkAnswer(correctIndex)
-
-}
-
-function checkAnswer(correctIndex) {
-    quiz.removeEventListener('click', answerClickHandler);
-    quiz.addEventListener('click', answerClickHandler);
-}
-
-function answerClickHandler(e) {
-    let selectedItem = e.target.closest('li');
-    if (!selectedItem) return;
-
-    let allItems = document.querySelectorAll('li');
-    let answers = [...document.querySelectorAll('li')];
-    let selectedIndex = answers.indexOf(selectedItem);
-
-    let checkAnswer = selectedIndex === correctIndex;
-
-    if (checkAnswer) {
-        selectedItem.classList.add('correct-answer');
-        answerCounter += 1;
-        updateCounter(answerCounter, '.answer-counter');
-    } else {
-        selectedItem.classList.add('incorrect-answer');
-    }
-
-    allItems.forEach(item => {
-        item.classList.add('not-allowed');
-    });
-}
-
-
-let nextSlide = (data) => {
-    button.addEventListener('click', () => {
-
-        if (pageCounter < 10) {
-
-            pageCounter += 1
-            updateCounter(pageCounter, '.page-counter')
-            renderQuestions(data.results[pageCounter])
+        if (index === 0) {
+            return allAnswers[index] = {
+                answer: item,
+                isCorrect: true
+            }
+        }
+        else {
+            return allAnswers[index] = {
+                answer: item,
+                isCorrect: false
+            }
         }
 
     })
 
+    for (i = allAnswers.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let memory = allAnswers[i]
+        allAnswers[i] = allAnswers[j]
+        allAnswers[j] = memory
+    }
 
+    let questionItem = document.createElement('h3')
+    questionItem.textContent = question
+    console.log(questionItem)
+
+    quiz.append(questionItem)
+
+    allAnswers.forEach(({ answer, isCorrect }) => {
+        let answerItem = document.createElement('li')
+        answerItem.textContent = answer
+        quiz.append(answerItem)
+        answerItem.addEventListener('click', () => {
+            checkAnswer(isCorrect, answerItem)
+        })
+    })
+
+}
+
+function checkAnswer(isCorrect, answerItem) {
+
+    if (isCorrect) {
+        answerItem.classList.add('correct-answer');
+        answerCounter += 1;
+        updateCounter(answerCounter, '.answer-counter');
+    }
+    else {
+        answerItem.classList.add('incorrect-answer');
+    }
+
+    let answersList = document.querySelectorAll('li')
+
+    answersList.forEach((item) => {
+        item.classList.add('not-allowed')
+    })
+
+}
+
+let nextSlide = (data) => {
+    button.addEventListener('click', () => {
+
+        quiz.innerHTML = ""
+
+        if (pageCounter < 10) {
+            pageCounter += 1
+            updateCounter(pageCounter, '.page-counter')
+            renderQuestions(data.results[slideIndex++])
+        }
+        else { quiz.innerHTML = `<h2>Quiz finished. Your score is ${answerCounter} / 10</h2>` }
+
+    })
 
 }
 
@@ -113,6 +115,5 @@ let updateCounter = (counter, counterContainer) => {
     }
 
 }
-
 
 init(apiUrl)
