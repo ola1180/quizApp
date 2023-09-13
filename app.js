1dfd922cf9927d8c29dd399676a71c91089b4119
Ola <aleksandra.kurdel@gmail.com>
2023-09-13 13:25:24 +0200

    code refactoring

--- app.js
+++ app.js
@@ -5,47 +5,40 @@ let answerCounter = 0
 let slideIndex = 0
 const localStoragePointsKey = 'quiz-points'
 let localStoragePoints = 0
-let timer = document.querySelector('.timer')
+const timer = document.querySelector('.timer')
 let countdown = null
 let rankPoints = []
+let quizData = null
 const localStorageRankPoints = 'rank-points'
 rankButton = document.querySelector('.rank')
 quizContainer = document.querySelector('.quiz')
 rankContainer = document.querySelector('.rank-container')
-let pointsWeight = [{ difficulty: 'easy', weight: 1 }, { difficulty: 'medium', weight: 2 }, { difficulty: 'hard', weight: 3 }]
-let weightMap = {
+const weightMap = {
     "easy": 1,
     "medium": 2,
     "hard": 3
 }
 
-async function init(apiUrl) {
+async function init() {
+    let response = await fetch(apiUrl)
 
-    fetch(apiUrl)
-        .then(response => {
-            if (response.status === 200) {
-                return response.json()
-            }
-            throw new Error()
-        })
-        .then((dataFromApi) => {
-            getPointsFromLocalStorage()
-            timerStart()
-            renderQuestions(dataFromApi.results[slideIndex])
-            nextSlide(dataFromApi)
-        })
-        .catch((error) => showError())
+    if (response.status !== 200) {
+        throw new Error()
+    }
 
+    const jsonResponse = await response.json()
+    quizData = jsonResponse
+
+    getPointsFromLocalStorage()
+    timerStart()
+    renderQuestions(jsonResponse.results[slideIndex])
+    rankRendering()
 }
 
-let renderQuestions = ({ question, correct_answer, difficulty, incorrect_answers: [...allIncorrectAnswers] }) => {
-
-    let allAnswers = ([correct_answer, ...allIncorrectAnswers]).map((item, index) => {
-
+const renderQuestions = ({ question, correct_answer, difficulty, incorrect_answers: [...allIncorrectAnswers] }) => {
+    const allAnswers = ([correct_answer, ...allIncorrectAnswers]).map((item, index) => {
         return { answer: item, isCorrect: (index === 0), level: difficulty }
-
     })
-
 
     for (i = allAnswers.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1))
@@ -54,121 +47,94 @@ let renderQuestions = ({ question, correct_answer, difficulty, incorrect_answers
         allAnswers[j] = memory
     }
 
-    let questionItem = document.createElement('h3')
+    const questionItem = document.createElement('h3')
     questionItem.textContent = question
-    let questionLevel = document.createElement('h3')
+    const questionLevel = document.createElement('h3')
     questionLevel.textContent = difficulty
-
-
-
     quiz.append(questionItem)
     quiz.append(questionLevel)
 
-
     allAnswers.forEach(({ answer, isCorrect, level }) => {
-        let answerItem = document.createElement('li')
-
+        const answerItem = document.createElement('li')
         answerItem.textContent = answer
-
         quiz.append(answerItem)
         answerItem.addEventListener('click', () => {
             checkAnswer(isCorrect, answerItem, level)
         })
     })
-
 }
 
-let checkAnswer = (isCorrect, answerItem, level) => {
-
+const checkAnswer = (isCorrect, answerItem, level) => {
     if (isCorrect) {
         answerItem.classList.add('correct-answer');
-
-        let weight = weightMap[level] ?? 1
+        const weight = weightMap[level] ?? 1
         answerCounter += weight
-
         updateCounter(answerCounter, '.answer-counter');
-    }
-
-    else {
+    } else {
         answerItem.classList.add('incorrect-answer');
     }
 
-    let answersList = document.querySelectorAll('li')
-
+    const answersList = document.querySelectorAll('li')
     answersList.forEach((item) => {
         item.classList.add('not-allowed')
     })
-
-
 }
 
-let nextSlide = (data) => {
-    button.addEventListener('click', () => {
+button.addEventListener('click', () => {
+    quiz.innerHTML = ""
 
-        quiz.innerHTML = ""
+    if (slideIndex < (quizData.results.length - 1)) {
+        timerStart()
+        slideIndex += 1
+        updateCounter(slideIndex + 1, '.page-counter')
+        renderQuestions(quizData.results[slideIndex])
+    } else {
+        clearInterval(countdown)
+        quiz.innerHTML = `<h2>Quiz finished. Your score is ${answerCounter} / 10</h2>`
+        updateLocalStorage(answerCounter)
+        button.classList.add('hidden')
+        timer.classList.add('hidden')
+    }
+})
 
-        if (slideIndex < 9) {
-            timerStart()
-            slideIndex += 1
-
-            updateCounter(slideIndex + 1, '.page-counter')
-            renderQuestions(data.results[slideIndex])
-
-        }
-        else {
-            clearInterval(countdown)
-            quiz.innerHTML = `<h2>Quiz finished. Your score is ${answerCounter} / 10</h2>`
-            updateLocalStorage(answerCounter)
-            button.classList.add('hidden')
-            timer.classList.add('hidden')
-
-        }
-
-    })
-
-}
-
-let showError = () => {
+const showError = () => {
     quiz.innerHTML = `<div class='error'>Data not found or thera are server problem</div>`
 }
 
-let updateCounter = (counter, counterContainer) => {
-    let counterElement = document.querySelector(counterContainer)
+const updateCounter = (counter, counterContainer) => {
+    const counterElement = document.querySelector(counterContainer)
+
     if (counterElement) {
         counterElement.textContent = `${counter} / 10`;
     }
 }
 
-let updateLocalStorage = (points) => {
+const updateLocalStorage = (points) => {
     localStoragePoints += points
     localStorage.setItem(localStoragePointsKey, localStoragePoints)
+
     rankPoints.push(points)
     localStorage.setItem(localStorageRankPoints, rankPoints)
 }
 
-let getPointsFromLocalStorage = () => {
-    let pointsFromLocalStorage = localStorage.getItem(localStoragePointsKey)
-    pointsFromLocalStorage === null ? pointsFromLocalStorage = 0 : localStoragePoints = parseInt(pointsFromLocalStorage)
-    let rankPointsLocalStorage = localStorage.getItem(localStorageRankPoints)
+const getPointsFromLocalStorage = () => {
+    const pointsFromLocalStorage = localStorage.getItem(localStoragePointsKey) ?? 0
+    localStoragePoints = parseInt(pointsFromLocalStorage)
+    const rankPointsLocalStorage = localStorage.getItem(localStorageRankPoints)
+
     if (rankPointsLocalStorage === null) {
         rankPoints = []
-    }
-    else {
+    } else {
         let splitedPoints = rankPointsLocalStorage.split(',')
         splitedPoints = splitedPoints.map((item) => {
-            let parsedItem = parseInt(item)
+            const parsedItem = parseInt(item)
             return rankPoints.push(parsedItem)
         })
     }
-
-
-
 }
 
-let timerStart = () => {
-
+const timerStart = () => {
     clearInterval(countdown)
-
     let count = 10
     timer.innerHTML = `${count} s`
 
@@ -181,23 +147,19 @@ let timerStart = () => {
             quiz.innerHTML = `<h2>Times out</h2>`
         }
     }, 1000)
-
-
 }
 
-
-rankButton.addEventListener('click', () => {
-    quizContainer.classList.add('hidden')
+const rankRendering = () => {
     sortedArray = rankPoints.sort().reverse()
     let rankRow = document.createElement('tr')
     let rankCol1 = document.createElement('td')
     let rankCol2 = document.createElement('td')
-
     rankCol1.textContent = 'Numer'
     rankCol2.textContent = 'Liczba punktów'
     rankRow.append(rankCol1)
     rankRow.append(rankCol2)
     rankContainer.append(rankRow)
+
     sortedArray.forEach((item, index) => {
         let rankItemRow = document.createElement('tr')
         let rankItemCol1 = document.createElement('td')
@@ -209,8 +171,11 @@ rankButton.addEventListener('click', () => {
         rankContainer.append(rankItemRow)
 
     })
+}
 
+rankButton.addEventListener('click', () => {
+    quizContainer.classList.add('hidden')
+    rankContainer.classList.remove('hidden')
 })
 
-
-init(apiUrl)
+init()
